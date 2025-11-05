2c61df5c360b2e1e24ef22964de2611e450aa880
jiayu <jiayu.hu01@gmail.com>
2025-11-06 10:23:20 +1100

    set up workspace

---
+++ index.js
@@ -0,0 +1,13 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("database/data_source.db");
+
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(8000, () => console.log("Server is running on Port 8000, visit http://localhost:8000/ to access your website"));
